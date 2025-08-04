9904d5a8813c8245176f5a4604cff90e1e710283
Anshika0506 <anshikakushwah143@gmail.com>
2025-08-04 11:56:24 +0530

    Update EditUser.js with https

--- fullstack-frontend/src/users/EditUser.js
+++ fullstack-frontend/src/users/EditUser.js
@@ -25,12 +25,12 @@ export default function EditUser() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.put(`http://fullstack-backend-2wke.onrender.com/user/${id}`, user);
+    await axios.put(`https://fullstack-backend-2wke.onrender.com/user/${id}`, user);
     navigate("/");
   };
 
   const loadUser = async () => {
-    const result = await axios.get(`http://fullstack-backend-2wke.onrender.com/user/${id}`);
+    const result = await axios.get(`https://fullstack-backend-2wke.onrender.com/user/${id}`);
     setUser(result.data);
   };
 
